4065fe7d9be2f3dd09900a7e7faad5a075dd21e3
Rahul Kumar <rahulpathak263@gmail.com>
2022-03-24 04:04:25 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 const header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Rahul Kumar</h1>
+        <h5 className='text-light'>Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className='me'>
+          <img src={ME} alt='me'/>
+        </div>
+      
+        <a href='#contact' className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
