cecb809f8d7641cc6ae894e7c1df84e9bf6c8468
Rahul Kumar <rahulpathak263@gmail.com>
2022-03-24 22:27:14 +0530

    Update experience.jsx

--- src/components/Experience/experience.jsx
+++ src/components/Experience/experience.jsx
@@ -1,9 +1,134 @@
 import React from 'react'
 import './Experience.css'
-const exp = () => {
+import {BsPatchCheckFill} from 'react-icons/bs'
+const experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>Skills I Have</h5>
+      <h2>My Experiences</h2>
+
+      <div className='container experience__container'>
+        <div className='experience__fullstack'>
+          <h3>Full-Stack Development</h3>
+          <div className="experience__content">
+            
+            <article className='experience__details'>
+            <BsPatchCheckFill className='experience__details-icons'/>
+            <div>
+            <h4>HTML</h4>
+            <small className='text-light'>Experienced</small>
+            </div>
+            </article>
+
+            <article className='experience__details'>
+            <BsPatchCheckFill className='experience__details-icons'/>
+            <div>
+            <h4>CSS</h4>
+            <small className='text-light'>Intermidiate</small>
+            </div>
+            </article>
+         
+          <article className='experience__details'>
+            <BsPatchCheckFill className='experience__details-icons'/>
+            <div>
+            <h4>JavaScript</h4>
+            <small className='text-light'>Intermidiate</small>
+            </div>
+            </article>
+          
+
+          <article className='experience__details'>
+            <BsPatchCheckFill  />
+            <div>
+            <h4>React Js</h4>
+            <small className='text-light'>Intermidiate</small>
+            </div>
+            </article>
+         
+
+          <article className='experience__details'>
+            <BsPatchCheckFill className='experience__details-icons'/>
+            <div>
+            <h4>MongoDB</h4>
+            <small className='text-light'>Intermidiated</small>
+            </div>
+            </article>
+        
+
+           <article className='experience__details'>
+           <BsPatchCheckFill className='experience__details-icons'/>
+           <div>
+           <h4>Node Js</h4>
+           <small className='text-light'>Intermidiate</small>
+           </div>
+           </article>
+         
+          </div>
+          </div>
+        
+        {/*end of full stack */}
+        
+        <div className='experience__datascience'>
+          <h3>Data Science</h3>
+          <div className="experience__content">
+            
+            <article className='experience__details'>
+            <BsPatchCheckFill className='experience__details-icons'/>
+            <div>
+            <h4>Python</h4>
+            <small className='text-light'>Experienced</small>
+            </div>
+            </article>
+
+            <article className='experience__details'>
+            <BsPatchCheckFill className='experience__details-icons'/>
+            <div>
+            <h4>ML</h4>
+            <small className='text-light'>Intermidiate</small>
+            </div>
+            </article>
+         
+          <article className='experience__details'>
+            <BsPatchCheckFill className='experience__details-icons'/>
+            <div>
+            <h4>SQL</h4>
+            <small className='text-light'>Intermidiate</small>
+            </div>
+            </article>
+          
+
+          <article className='experience__details'>
+            <BsPatchCheckFill className='experience__details-icons'/>
+            <h4>TensorFlow</h4>
+            <small className='text-light'>Intermidiate</small>
+            </article>
+         
+
+          <article className='experience__details'>
+            <BsPatchCheckFill className='experience__details-icons'/>
+            <div>
+            <h4>Docker</h4>
+            <small className='text-light'>Intermidiated</small>
+            </div>
+            </article>
+        
+
+           <article className='experience__details'>
+           <BsPatchCheckFill className='experience__details-icons'/>
+           <div>
+             <h4>NLP/ComputerVision</h4>
+           <small className='text-light'>Intermidiate</small>
+           </div>
+           </article>
+          
+          </div>
+          </div> 
+        </div>
+        
+
+        
+    </section>
   )
 }
 
-export default exp
+export default experience
