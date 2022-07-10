2af7f4ebfdb38451527bc3c2b25ebbbb89c1a3b8
Rahul Kumar <rahulpathak263@gmail.com>
2022-07-10 17:41:28 +0530

    Update portfolio.jsx

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -4,8 +4,13 @@ import IMG1 from '../../assets/portfolio1.jpg'
 const portfolio = () => {
   return (
     <section id='portfolio'>
-      
+      <h5> My Recent Work </h5>
+    `<h2>Portfolio</h2>`
 
+
+
+
+  
     </section>
   )
 }
